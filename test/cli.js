c2c193c451bd9f89a59ebf30f6f0e1055a64dca4
Kyle Shockey <shockey@users.noreply.github.com>
2019-07-20 20:33:31 -0500

    add CLI tests

---
+++ test/cli.js
@@ -0,0 +1,25 @@
+const ChildProcess = require("child_process")
+var assert = require('assert');
+
+// placed here to make multiline indentation easier, feel free to move if you're
+// adding more tests here
+const desiredOutput = `<h3>This is a simple Markdown document</h3>
+<blockquote>
+<p>It’s really just here to make sure the Remarkable CLI can take a Markdown
+document as an input</p>
+</blockquote>
+<p><strong>It’s not comprehensive or anything</strong></p>
+<p>Hopefully the CLI can handle it though</p>
+`
+
+describe("Remarkable CLI", function() {
+  it("simple Markdown file as input", function() {
+    const output = ChildProcess.execSync(
+      "../bin/remarkable.js ./fixtures/cli-input.md", 
+      {
+        cwd: __dirname
+      }
+    )
+    assert.strictEqual(output.toString(), desiredOutput)
+  })
+})
---
+++ test/fixtures/cli-input.md
@@ -0,0 +1,8 @@
+### This is a simple Markdown document
+
+> It's really just here to make sure the Remarkable CLI can take a Markdown
+> document as an input
+
+**It's not comprehensive or anything**
+
+Hopefully the CLI can handle it though
